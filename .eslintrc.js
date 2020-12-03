0485ff923b4c953af569c02bab593661bdb52b36
talltotal <talltotal@163.com>
2020-12-03 18:35:26 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    root: true,
+    env: {
+        node: true,
+        es6: true
+    },
+    rules: {
+        'indent': ['error', 4],
+    },
+}
